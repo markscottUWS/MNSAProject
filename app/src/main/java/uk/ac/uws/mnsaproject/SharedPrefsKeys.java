c8ff3146e67996d077bf473ccdbebd717daaeeac
Richysteez <44305449+Richysteez@users.noreply.github.com>
2018-11-20 14:05:53 +0000

    Create SharedPrefsKeys.java
    Shared Preferences key strings

---
+++ app/src/main/java/uk/ac/uws/mnsaproject/SharedPrefsKeys.java
@@ -0,0 +1,13 @@
+package uk.ac.uws.mnsaproject;
+
+public class SharedPrefsKeys
+{
+    public static final String nameKey = "name_key";
+    public static final String heightKey = "height_key";
+    public static final String weightKey = "weight_key";
+    public static final String idealWeightKey = "ideal_weight_key";
+    public static final String genderKey = "gender_key";
+    public static final String expectationKey = "expectation_key";
+    public static final String ageKey = "age_key";
+    public static final String activityKey = "activity_key";
+}
