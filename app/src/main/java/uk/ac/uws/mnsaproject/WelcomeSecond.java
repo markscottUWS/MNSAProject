4acb339c5883140ab7f2ae869d492966b9de531c
lizapoll <44290802+lizapoll@users.noreply.github.com>
2018-11-23 04:13:59 +0000

    Update WelcomeSecond.java

--- app/src/main/java/uk/ac/uws/mnsaproject/WelcomeSecond.java
+++ app/src/main/java/uk/ac/uws/mnsaproject/WelcomeSecond.java
@@ -1,122 +1,51 @@
 package uk.ac.uws.mnsaproject;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+public class WelcomeSecond extends AppCompatActivity {
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link WelcomeSecond.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link WelcomeSecond#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class WelcomeSecond extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    EditText editText;
+    Button button;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private String userName;
-    private TextView welcomeTextView;
-
-    private OnFragmentInteractionListener mListener;
-
-    public WelcomeSecond() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment WelcomeSecond.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static WelcomeSecond newInstance(String param1, String param2) {
-        WelcomeSecond fragment = new WelcomeSecond();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    String password;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        setContentView(R.layout.fragment_welcome_second);
 
-        welcomeTextView = getView().findViewById(R.id.welcome_back_textview);
+        SharedPreferences settings = getSharedPreferences("PREFS", 0);
+        password = settings.getString("password","");
 
-        SharedPreferences prefs = getActivity().getSharedPreferences(SharedPrefsKeys.prefsId, Context.MODE_PRIVATE);
-        userName = prefs.getString(SharedPrefsKeys.nameKey, "");
-        if(userName != "")
-        {
-            welcomeTextView.setText(getString(R.string.welcome_name, userName));
-        }
-    }
+        editText = (EditText) findViewById(R.id.editText);
+        button = (Button) findViewById(R.id.button);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome_second, container, false);
-    }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
+        button.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = editText.getText().toString();
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
+                if (text.equals(password)) {
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
+                    Intent intent = new Intent(getApplicationContext(), DetailsActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Toast.makeText(WelcomeSecond.this, "Wrong password!", Toast.LENGTH_SHORT).show();
+                }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+            }
+        });
     }
 }
+
