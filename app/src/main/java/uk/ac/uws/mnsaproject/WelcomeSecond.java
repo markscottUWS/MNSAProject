2e9df983a8af10f9be107aca1790ada806ebe05b
Mark Scott <marsscotia@googlemail.com>
2018-11-23 20:36:03 +0000

    personalised second welcome screen
    Added user name and weeks joined to second welcome screen

--- app/src/main/java/uk/ac/uws/mnsaproject/WelcomeSecond.java
+++ app/src/main/java/uk/ac/uws/mnsaproject/WelcomeSecond.java
@@ -9,14 +9,24 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
 
 public class WelcomeSecond extends AppCompatActivity {
 
     EditText editText;
     Button button;
+    private TextView welcomeBackTextView;
+    private TextView weekTextView;
 
     String password;
+
+    private String name;
+    private int weeksJoined;
+    private boolean validValues;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +38,15 @@ public class WelcomeSecond extends AppCompatActivity {
 
         editText = (EditText) findViewById(R.id.editText);
         button = (Button) findViewById(R.id.button);
+        welcomeBackTextView = findViewById(R.id.welcome_back_textview);
+        weekTextView = findViewById(R.id.week_textview);
 
+        retrievePrefs();
+        if(validValues)
+        {
+            welcomeBackTextView.setText(getString(R.string.welcome_name, name));
+            weekTextView.setText(getString(R.string.in_week, weeksJoined));
+        }
 
         button.setOnClickListener(new OnClickListener() {
             @Override
@@ -46,6 +64,37 @@ public class WelcomeSecond extends AppCompatActivity {
 
             }
         });
+
+
+    }
+
+    private void retrievePrefs()
+    {
+        validValues = true;
+        SharedPreferences prefs = getSharedPreferences(SharedPrefsKeys.prefsId, MODE_PRIVATE);
+        name = prefs.getString(SharedPrefsKeys.nameKey, "");
+        if(name.equals(""))
+        {
+            validValues = false;
+        }
+        long joinTimeInMillis = prefs.getLong(SharedPrefsKeys.joinDateMillisKey, 0);
+        if(joinTimeInMillis > 0)
+        {
+            calculateWeeks(joinTimeInMillis);
+        }
+        else
+        {
+            validValues = false;
+        }
+    }
+
+    private void calculateWeeks(long joinTimeInMillis)
+    {
+        Calendar now = Calendar.getInstance();
+        long nowMillis = now.getTimeInMillis();
+        long difference = nowMillis - joinTimeInMillis;
+        long daysJoined = TimeUnit.DAYS.convert(difference, TimeUnit.MILLISECONDS);
+        weeksJoined = (int)((daysJoined / 7) + 1);
     }
 }
 
