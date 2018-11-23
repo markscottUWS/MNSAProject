a4957c2eb74088d5a2d595ff12282e6ac7fe5364
lizapoll <44290802+lizapoll@users.noreply.github.com>
2018-11-23 01:44:54 +0000

    create SplashActivity.java

---
+++ app/src/main/java/uk/ac/uws/mnsaproject/SplashActivity.java
@@ -0,0 +1,31 @@
+package uk.ac.uws.mnsaproject;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.PixelFormat;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
