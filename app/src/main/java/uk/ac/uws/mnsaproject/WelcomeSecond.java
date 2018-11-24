7f31a9f2604768c7fddc693b7013d4d48329e7f6
Mark Scott <marsscotia@googlemail.com>
2018-11-24 14:31:14 +0000

    Updated second welcome screen
    When password is entered on second welcome screen, password UI disappears and nav UI appears

--- app/src/main/java/uk/ac/uws/mnsaproject/WelcomeSecond.java
+++ app/src/main/java/uk/ac/uws/mnsaproject/WelcomeSecond.java
@@ -3,24 +3,33 @@ package uk.ac.uws.mnsaproject;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ScaleDrawable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Calendar;
 import java.util.concurrent.TimeUnit;
 
-public class WelcomeSecond extends AppCompatActivity {
+public class WelcomeSecond extends AppCompatActivity implements View.OnClickListener{
 
     EditText editText;
     Button button;
+    TextView textView;
     private TextView welcomeBackTextView;
     private TextView weekTextView;
+    private ImageButton waterButton;
+    private ImageButton planButton;
+    private ImageButton detailsButtons;
+    private ConstraintLayout navLayout;
 
     String password;
 
@@ -36,10 +45,22 @@ public class WelcomeSecond extends AppCompatActivity {
         SharedPreferences settings = getSharedPreferences("PREFS", 0);
         password = settings.getString("password","");
 
-        editText = (EditText) findViewById(R.id.editText);
-        button = (Button) findViewById(R.id.button);
+        editText = findViewById(R.id.editText);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
         welcomeBackTextView = findViewById(R.id.welcome_back_textview);
         weekTextView = findViewById(R.id.week_textview);
+        waterButton = findViewById(R.id.water_button);
+        detailsButtons = findViewById(R.id.details_button);
+        planButton = findViewById(R.id.plan_button);
+        navLayout = findViewById(R.id.nav_layout);
+
+        navLayout.setVisibility(View.INVISIBLE);
+
+        button.setOnClickListener(this);
+        waterButton.setOnClickListener(this);
+        planButton.setOnClickListener(this);
+        detailsButtons.setOnClickListener(this);
 
         retrievePrefs();
         if(validValues)
@@ -47,24 +68,6 @@ public class WelcomeSecond extends AppCompatActivity {
             welcomeBackTextView.setText(getString(R.string.welcome_name, name));
             weekTextView.setText(getString(R.string.in_week, weeksJoined));
         }
-
-        button.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String text = editText.getText().toString();
-
-                if (text.equals(password)) {
-
-                    Intent intent = new Intent(getApplicationContext(), DetailsActivity.class);
-                    startActivity(intent);
-                    finish();
-                } else {
-                    Toast.makeText(WelcomeSecond.this, "Wrong password!", Toast.LENGTH_SHORT).show();
-                }
-
-            }
-        });
-
 
     }
 
@@ -95,6 +98,52 @@ public class WelcomeSecond extends AppCompatActivity {
         long difference = nowMillis - joinTimeInMillis;
         long daysJoined = TimeUnit.DAYS.convert(difference, TimeUnit.MILLISECONDS);
         weeksJoined = (int)((daysJoined / 7) + 1);
+    }
+
+
+    @Override
+    public void onClick(View view)
+    {
+        switch(view.getId())
+        {
+            case R.id.button:
+            {
+                String text = editText.getText().toString();
+
+                if (text.equals(password))
+                {
+                    navLayout.setVisibility(View.VISIBLE);
+                    textView.setVisibility(View.GONE);
+                    button.setVisibility(View.GONE);
+                    editText.setVisibility(View.GONE);
+                }
+                else
+                {
+                    Toast.makeText(WelcomeSecond.this, "Wrong password!", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            }
+            case R.id.water_button:
+            {
+                break;
+            }
+            case R.id.details_button:
+            {
+                Intent detailsIntent = new Intent(getApplicationContext(), DetailsActivity.class);
+                startActivity(detailsIntent);
+                break;
+            }
+            case R.id.plan_button:
+            {
+                Intent planIntent = new Intent(getApplicationContext(), PlanActivity.class);
+                startActivity(planIntent);
+                break;
+            }
+            default:
+            {
+                break;
+            }
+        }
     }
 }
 
